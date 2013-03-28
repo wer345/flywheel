78e004d13dda88be20a7aa8ba31c8d2550db4ee4
Fujun Wu <wu_fujun@yahoo.com>
2013-03-27 22:22:30 -0700

    added gsl

---
+++ gsl/example/example.cpp
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_multimin.h>
+
+     /* Paraboloid centered on (p[0],p[1]), with  
+        scale factors (p[2],p[3]) and minimum p[4] */
+     
+     double
+     my_f (const gsl_vector *v, void *params)
+     {
+       double x, y;
+       double *p = (double *)params;
+       
+       x = gsl_vector_get(v, 0);
+       y = gsl_vector_get(v, 1);
+      
+       return p[2] * (x - p[0]) * (x - p[0]) +
+                p[3] * (y - p[1]) * (y - p[1]) + p[4]; 
+     }
+     
+        int 
+     main(void)
+     {
+       double par[5] = {1.0, 2.0, 10.0, 20.0, 30.0};
+     
+       const gsl_multimin_fminimizer_type *T = 
+         gsl_multimin_fminimizer_nmsimplex;
+       gsl_multimin_fminimizer *s = NULL;
+       gsl_vector *ss, *x;
+       gsl_multimin_function minex_func;
+     
+       size_t iter = 0;
+       int status;
+       double size;
+     
+       /* Starting point */
+       x = gsl_vector_alloc (2);
+       gsl_vector_set (x, 0, 5.0);
+       gsl_vector_set (x, 1, 7.0);
+     
+       /* Set initial step sizes to 1 */
+       ss = gsl_vector_alloc (2);
+       gsl_vector_set_all (ss, 1.0);
+     
+       /* Initialize method and iterate */
+       minex_func.n = 2;
+       minex_func.f = my_f;
+       minex_func.params = par;
+     
+       s = gsl_multimin_fminimizer_alloc (T, 2);
+       gsl_multimin_fminimizer_set (s, &minex_func, x, ss);
+     
+       do
+         {
+           iter++;
+           status = gsl_multimin_fminimizer_iterate(s);
+           
+           if (status) 
+             break;
+     
+           size = gsl_multimin_fminimizer_size (s);
+           status = gsl_multimin_test_size (size, 1e-2);
+     
+           if (status == GSL_SUCCESS)
+             {
+               printf ("converged to minimum at\n");
+             }
+     
+           printf ("%5d %10.3e %10.3e f() = %7.3f size = %.3f\n", 
+                   iter,
+                   gsl_vector_get (s->x, 0), 
+                   gsl_vector_get (s->x, 1), 
+                   s->fval, size);
+         }
+       while (status == GSL_CONTINUE && iter < 100);
+       
+       gsl_vector_free(x);
+       gsl_vector_free(ss);
+       gsl_multimin_fminimizer_free (s);
+     
+       return status;
+     }
+
+////////////////////////////////////////////////////////////
+// Solve Ax = b with LU and cholesky
+int main0(int argc, char **argv)
+{
+    printf("=========== tst2 ===========\n");
+    double a_data[] = { 2,1,1,3,2,
+                        1,2,2,1,1,
+                        1,2,9,1,5,
+                        3,1,1,7,1,
+                        2,1,5,1,8 };
+
+    double b_data[] = { -2,4,3,-5,1 };
+
+    gsl_vector *x = gsl_vector_alloc (5);
+    gsl_permutation * p = gsl_permutation_alloc (5);
+
+    gsl_matrix_view m 
+        = gsl_matrix_view_array(a_data, 5, 5);
+
+    gsl_vector_view b
+        = gsl_vector_view_array(b_data, 5);
+
+
+    int s;
+
+    gsl_linalg_LU_decomp (&m.matrix, p, &s);
+
+    gsl_linalg_LU_solve (&m.matrix, p, &b.vector, x);
+
+    printf ("x = \n");
+    gsl_vector_fprintf(stdout, x, "%g");
+
+    double a2_data[] = { 2,1,1,3,2,
+                        1,2,2,1,1,
+                        1,2,9,1,5,
+                        3,1,1,7,1,
+                        2,1,5,1,8 };
+
+    double b2_data[] = { -2,4,3,-5,1 };
+    gsl_matrix_view m2 = gsl_matrix_view_array(a2_data, 5, 5);
+    gsl_vector_view b2 = gsl_vector_view_array(b2_data, 5);
+    gsl_linalg_cholesky_decomp(&m2.matrix);
+    gsl_linalg_cholesky_solve(&m2.matrix, &b2.vector, x);
+    printf ("x = \n");
+    gsl_vector_fprintf(stdout, x, "%g");
+
+    gsl_permutation_free (p);
+    gsl_vector_free(x);
+	return 0;
+}
