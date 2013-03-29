d198c1ba76527b0f9cf90b9be4c2f996cdec12af
unknown <c_fujunw@C-FUJUNW.na.qualcomm.com>
2013-03-28 20:02:51 -0700

    add fit and self-build gsl 1.15 with visual stodio 2010

--- deflection/main.cpp
+++ deflection/main.cpp
@@ -1,10 +1,96 @@
+//#define hypot gsl_hypot
+
 #include <stdio.h>
 #include <iostream>
 
 #include <gsl/gsl_linalg.h>
 #include <gsl/gsl_multimin.h>
+#include <gsl/gsl_multifit.h>
 
 #include "Poly.h"
+
+ double testfunc(double x)
+ {
+	 return cos(x);
+ }
+ 
+ int fit()
+     {
+       int i, n;
+       double xi, yi, ei, chisq;
+       gsl_matrix *X, *cov;
+       gsl_vector *y, *w, *c;
+     
+       n = 9;
+	   int p=7;
+     
+       X = gsl_matrix_alloc (n, p);
+       y = gsl_vector_alloc (n);
+       w = gsl_vector_alloc (n);
+     
+       c = gsl_vector_alloc (p);
+       cov = gsl_matrix_alloc (p, p);
+     
+       for (i = 0; i < n; i++)
+         {
+		   xi=3.1415926/4.0*i;
+			yi=testfunc(xi);
+			ei=0.01;
+     
+           printf ("%g %g +/- %g\n", xi, yi, ei);
+
+		   double v=1.0;
+		   for(int j=0;j<p;j++) {
+	           gsl_matrix_set (X, i, j, v);
+			   v*=xi;
+		   }
+/*
+           gsl_matrix_set (X, i, 0, 1.0);
+           gsl_matrix_set (X, i, 1, xi);
+           gsl_matrix_set (X, i, 2, xi*xi);
+*/           
+           gsl_vector_set (y, i, yi);
+           gsl_vector_set (w, i, 1.0/(ei*ei));
+         }
+     
+       {
+         gsl_multifit_linear_workspace * work 
+           = gsl_multifit_linear_alloc (n, p);
+//         gsl_multifit_linear (X, y, c, cov,&chisq, work);
+         gsl_multifit_wlinear (X, w, y, c, cov,&chisq, work);
+         gsl_multifit_linear_free (work);
+       }
+     
+     #define C(i) (gsl_vector_get(c,(i)))
+     #define COV(i,j) (gsl_matrix_get(cov,(i),(j)))
+     
+       {
+         printf ("# best fit: Y = %g + %g X + %g X^2\n", C(0), C(1), C(2));
+		   printf("# best fit: Y=");
+		   for(int i=0;i<p;i++) {
+			   if(C(i)>=0) 
+				   printf("+");
+			   printf("%g",C(i));
+			   int j=i;
+			   while(j-->0)
+				   printf("*x");
+
+		   }
+         printf ("\n# chisq = %g\n", chisq);
+       }
+     
+       gsl_matrix_free (X);
+       gsl_vector_free (y);
+       gsl_vector_free (w);
+       gsl_vector_free (c);
+       gsl_matrix_free (cov);
+     
+       return 0;
+     }
+
+
+
+
 // (x'y''-x''y')^2 = (x'^2+y'^2)^3 *{[(xe-2x)*Fy-(ye-2y)Fx)]/(2EI)}^2
 
      /* Paraboloid centered on (p[0],p[1]), with  
@@ -22,10 +108,9 @@
        return p[2] * (x - p[0]) * (x - p[0]) +
                 p[3] * (y - p[1]) * (y - p[1]) + p[4]; 
      }
-     
- int main(void)
-{
 
+void test_poly()
+{
 	Poly a= Poly(3);
 	Poly b= Poly(3);
 	a.m_c[0]=1;
@@ -60,8 +145,13 @@
 	double v1=1/(1+x0+x0*x0);
 	double v2=inv.value(x0);
 	cout<<"v1=" << v1<<"; v2=" << v2<<endl;
+}
 
+ int main(void)
+{
 
+//	test_poly();
+	fit();
 	char ch=getchar();
 //	char in;
 //	cin>>in;
